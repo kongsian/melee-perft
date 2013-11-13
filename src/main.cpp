f8109f2fc860d6a0b25b697ef437a219efcd4458
Chua Kong Sian <kongsian@yahoo.com>
2013-11-13 18:42:27 +0800

    Add a main() function.

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+//--------------------------------------------------------------------//
+//
+//  Melee Perft
+//  Copyright (C) 2013 Chua Kong Sian.
+//
+//  This file is part of Melee Perft.
+//
+//  Melee Perft is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Melee Perft is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Melee Perft.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------//
+
+//--------------------------------------------------------------------//
+//  main.cpp
+//--------------------------------------------------------------------//
+
+//--------------------------------------------------------------------//
+//  includes
+//--------------------------------------------------------------------//
+
+#include "program.h"
+
+#include <iostream>
+
+//--------------------------------------------------------------------//
+//  functions
+//--------------------------------------------------------------------//
+
+int main()
+{
+    std::cout << Program::Name
+              << ' '
+              << Program::Version
+              << " by "
+              << Program::Author
+              << std::endl;
+}
